267b505bc18a47aeb874e5abf958981806d14af2
math-araujo <araujo_m@usp.br>
2022-11-01 23:10:47 -0300

    Add project initial structure

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <imgui-SFML.h>
+#include <imgui.h>
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int main()
+{
+}
